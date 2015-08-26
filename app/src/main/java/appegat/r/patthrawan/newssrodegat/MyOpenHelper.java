4746433dbbc4cda3cb630bf3be9f64661d9b500c
unknown <patthrawan@gmail.com>
2015-08-26 16:11:21 +0700

    OpenHelper Finish

---
+++ app/src/main/java/appegat/r/patthrawan/newssrodegat/MyOpenHelper.java
@@ -0,0 +1,35 @@
+package appegat.r.patthrawan.newssrodegat;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by ADMIN-IT on 26/8/2558.
+ */
+public class MyOpenHelper extends SQLiteOpenHelper {
+
+    //Explicit
+    private static final String DATABASE_NAME = "srod.db";
+    private static final int DATABASE_VERSION = 1;
+    private static final String CREATE_USER_TABLE = "create table userTABLE " +
+            "(_id integer primary key, Password text, Name text);";
+    private static final String CREATE_NEWS_TABLE = "create table newsTABLE " +
+            "(_id integer primary key, Date text, Head text, Detail text, Image text, Owner text);";
+
+    public MyOpenHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+
+    }   //Constructor
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        sqLiteDatabase.execSQL(CREATE_USER_TABLE);
+        sqLiteDatabase.execSQL(CREATE_NEWS_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}   //Main Class
